bcc78c044c30fbc7ffbef400af436f7eea5dd458
Dain Kim <dev.dain.k@gmail.com>
2020-04-18 23:52:26 +0900

    자바스크립트 (welcome 완료, qna 진행중)

---
+++ js/data.js
@@ -0,0 +1,97 @@
+const qnaList = [
+    {
+        q: '1. 하루 중 최상의 컨디션인 시간은?',
+        a: [
+            {answer: 'a. 아침', score: 2},
+            {answer: 'b. 낮부터 이른 저녁', score: 4},
+            {answer: 'c. 늦은 밤', score: 6}
+        ]
+    },
+    {
+        q: '2. 보통 어떻게 걸으시나요?',
+        a: [
+            {answer: 'a. 큰 보폭으로 빨리', score: 6},
+            {answer: 'b. 좁은 보폭으로 빨리', score: 4},
+            {answer: 'c. 고개를 들고 보통 속도로', score: 7},
+            {answer: 'd. 고개를 숙이고 보통 속도로', score: 2},
+            {answer: 'e. 느린 속도로', score: 1}
+        ]
+    },
+    {
+        q: '3. 사람들과 대화할 때 당신의 제스처는?',
+        a: [
+           {answer: 'a. 팔짱을 끼고 있다', score: 4},
+           {answer: 'b. 깍지를 끼고 있다', score: 2},
+           {answer: 'c. 한 손이나 양손을 주머니에 넣거나 엉덩이를 짚는다', score: 5},
+           {answer: 'd. 대화하는 사람을 툭툭 치거나 건드린다', score: 7}, 
+           {answer: 'e. 귀나 턱, 머리카락을 만진다', score: 6}
+        ]
+    },
+    {
+        q: '4. 앉아서 쉴 때 어떻게 앉으시나요?',
+        a: [
+            {answer: 'a. 다리를 가지런히 하고 단정히 앉는다', score: 4},
+            {answer: 'b. 다리를 꼬고 앉는다', score: 6},
+            {answer: 'c. 다리를 쭉 뻗고 앉는다', score: 2},
+            {answer: 'd. 한쪽 다리를 접어 깔고 앉는다', score: 1}
+        ]
+    },
+    {
+        q: '5. 정말 즐거운 일이 생겼을 때 어떻게 반응하시나요?',
+        a: [
+            {answer: 'a. 아주 크게 웃는다', score: 6},
+            {answer: 'b. 웃긴 하지만 엄청 크진 않다', score: 4},
+            {answer: 'c. 조용히 미소 짓는다', score: 3},
+            {answer: 'd. 희미하게 겸연쩍은 미소를 짓는다', score: 5}
+        ]
+    },
+    {
+        q: '6. 파티나 모임에 갔을 때 어떻게 행동하시나요?',
+        a: [
+            {answer: 'a. 임팩트 있게 등장해 모두 내가 온 걸 알아차리게 한다', score: 6},
+            {answer: 'b. 조용히 들어가서 아는 사람이 있는지 둘러본다', score: 4},
+            {answer: 'c. 최대한 조용히 들어가서 아무도 내가 있는지 모르게 한다', score: 2}
+        ]
+    },
+    {
+        q: '7. 정말 열심히 집중하고 있는 당신. 누군가 끼어들거나 말을 건다면?',
+        a: [
+            {answer: 'a. 휴식이 반갑다', score: 6},
+            {answer: 'b. 너무 짜증난다', score: 2},
+            {answer: 'c. 상황에 따라 다르다', score: 4}
+        ]
+    },
+    {
+        q: '8. 어떤 색깔을 가장 좋아하시나요?',
+        a: [
+            {answer: 'a. 빨강, 주황', score: 6},
+            {answer: 'b. 검정', score: 7},
+            {answer: 'c. 노랑, 하늘색', score: 5},
+            {answer: 'd. 초록', score: 4},
+            {answer: 'e. 남색, 보라', score: 3},
+            {answer: 'f. 하양', score: 2},
+            {answer: 'g. 갈색, 회색', score: 1}
+        ]
+    },
+    {
+        q: '9. 밤에 잠들기 직전에 어떻게 눕나요?',
+        a: [
+            {answer: 'a. 천장을 보고 똑바로 눕는다', score: 7},
+            {answer: 'b. 배를 바닥에 깔고 엎드려 잔다', score: 6},
+            {answer: 'c. 옆으로 살짝 돌아 누워서 잔다', score: 4},
+            {answer: 'd. 한쪽 팔로 머리를 베고 잔다', score: 2},
+            {answer: 'e. 머리 끝까지 이불을 덮는다', score: 1}
+        ]
+    },
+    {
+        q: '10. 어떤 꿈을 자주 꾸시나요?',
+        a: [
+            {answer: 'a. 아래로 떨어지는 꿈', score: 4},
+            {answer: 'b. 싸우는 꿈', score: 2},
+            {answer: 'c. 사람이나 물건을 찾는 꿈', score: 3},
+            {answer: 'd. 하늘을 날거나 물 위에 떠다니는 꿈', score: 5},
+            {answer: 'e. 거의 꿈을 안 꾸고 잔다', score: 6},
+            {answer: 'f. 항상 좋은 꿈만 꾼다', score: 1}
+        ]
+    }
+]
--- js/main.js
+++ js/main.js
@@ -1,22 +1,100 @@
-function start() {
-    const header = document.getElementById('header');
-    const footer = document.getElementById('footer');
-    const welcome = document.getElementById('welcome');
-    header.style.animation = 
-        'fade-out 0.3s ease-in-out forwards, '+
-        'going-up 0.3s ease-in-out forwards';
-    footer.style.opacity = 0;
-    welcome.style.animation = 
-        'fade-out 0.3s ease-in-out forwards, '+
-        'going-up 0.3s ease-in-out forwards';
-    header.style.display = 'none';
-    footer.style.display = 'none';
-    welcome.style.display = 'none';
+const STATUS = 10;
+let qIdx = -1;
+let score = 0;
+let beginFlag = false;
+let select = [];
+
+function end() {
+    
+}
+
+function addAnswer(answerTxt, idx) {
+    let answer = document.createElement('button');
+    const a = document.querySelector('.answer');
+    addClass(answer, 'a box');
+    answer.innerHTML = answerTxt;
+    console.log(answer);
+
+    answer.addEventListener('click', function() {
+        select[qIdx] = idx;
+            aBox = document.querySelectorAll('.a box');
+            console.log(aBox);
+            for (let i = 0; i < aBox.length; i++) {
+                console.log('??');
+                aBox[i].style.animation = 
+                'going-left 0.5s forwards, '
+                'fade-out 0.5s forwards';
+            }   //감사합니다 감사합니다ㅋㅋㅋㅋㅋㅋㅋ
+        setTimeout(function() {   
+            goNext();
+        a.innerHTML = '';   
+            
+        }, 1000);
+        
+    });
+
+    answer.style.animation = 
+        'going-down 0.3s forwards, fade-in 0.3s forwards';
+
+    answer.display = 'none';
+    a.appendChild(answer);
+}
+
+function addClass(elem, className) {
+    elem.className += " "+className;
+}
+
+function goNext() {
+    if (qIdx++ === qnaList.length) {
+        end();
+        return;
+    }
     const qna = document.getElementById('qna');
     qna.style.display = 'block';
+    console.log(qIdx);
+    /*
+    status = document.querySelector('.status');
+    status.style.width = (STATUS * (idx+1))+'%';
+    */
+    qNum = qnaList[qIdx];
+    const q = document.querySelector('.q');
+    q.innerHTML = qNum.q;
     qna.style.animation = 
         'fade-in 0.3s ease-in-out 0.5s forwards, '+
         'going-down 0.3s ease-in-out 0.5s forwards';
+    setTimeout(function() {
+        const endIdx = qNum.a.length-1;
+        for (let i in qNum.a) {
+            console.log(i);
+            addAnswer(qNum.a[i].answer, i);
+        }
+    }, 1000);
+}
+
+function begin() {
+    if (beginFlag)
+        return;
+    beginFlag = true;
+
+    const header = document.getElementById('header');
+    const footer = document.getElementById('footer');
+    const welcome = document.getElementById('welcome');
+
+    header.style.animation = 
+        'going-up 0.4s forwards, '+
+        'fade-out 0.4s forwards';
+    welcome.style.animation = 
+        'going-up 0.4s ease-in-out 0.5s forwards, '+
+        'fade-out 0.4s ease-in-out 0.5s forwards';
+    footer.style.animation = 
+        'going-down 0.4s forwards, '+
+        'fade-out 0.4s forwards';
+    setTimeout(function() {
+        header.style.display = 'none';
+        footer.style.display = 'none';
+        welcome.style.display = 'none';
+        goNext();
+    }, 1000);
 }
 
 function load() {
@@ -37,7 +115,7 @@ function load() {
             msg.innerHTML = '이름을 입력하고 시작해 주세요.';
         } else {
             msg.innerHTML = '';
-            start();
+            begin();
         }
     });
     
