cb2a403a01c3b86ad5672d2f0e8e805e26f5562a
Dain Kim <dev.dain.k@gmail.com>
2020-04-19 14:49:03 +0900

    qna, calc js 끝 (점수 계산과 data 정리 필요)

--- js/main.js
+++ js/main.js
@@ -1,74 +1,117 @@
-const STATUS = 10;
+const header = document.getElementById('header');
+const footer = document.getElementById('footer');
+const qna = document.getElementById('qna');
+const ENDPOINT = 10;
 let qIdx = -1;
 let score = 0;
 let beginFlag = false;
 let select = [];
 
+function goResult() {
+    const result = document.getElementById('result');
+    setTimeout(function() {
+        header.style.display = 'block';
+        footer.style.display = 'block';
+        result.style.display = 'block';
+        header.style.animation = 
+            'fade-in 0.3s forwards';
+        footer.style.animation = 
+            'fade-in 0.3s forwards';
+        result.style.animation = 
+            'going-up 0.5s forwards, '+
+            'fade-in 0.5s forwards';
+    }, 600);
+}
+
 function end() {
-    
+    qna.style.animation ='';
+    const interval = setInterval(function() {
+        qna.style.opacity -= 0.1;
+        qna.style.transform = 'translateY(-1px)';
+    }, 50);
+    setTimeout(function() {
+        clearTimeout(interval);
+    }, 500);
+    setTimeout(function() {
+        qna.style.display = 'none';
+    }, 500);
+    setTimeout(function() {
+        const calc = document.getElementById('calc');
+        calc.style.display = 'block';
+        calc.style.animation =
+            'going-up 0.5s forwards, '+
+            'fade-in 0.5s forwards';
+    }, 700);
+    setTimeout(function() {
+        calc.style.animation ='';
+        calc.style.animation = 
+            'going-left 0.4s forwards, '+
+            'fade-out 0.4s forwards';
+        setTimeout(function() {
+            calc.style.display = 'none';
+            goResult();
+        }, 400);
+    }, 9000);
 }
 
 function addAnswer(answerTxt, idx) {
     let answer = document.createElement('button');
     const a = document.querySelector('.answer');
-    addClass(answer, 'a box');
+    answer.className += 'a box';
     answer.innerHTML = answerTxt;
-    console.log(answer);
-
     answer.addEventListener('click', function() {
-        select[qIdx] = idx;
-            aBox = document.querySelectorAll('.a box');
-            console.log(aBox);
-            for (let i = 0; i < aBox.length; i++) {
-                console.log('??');
-                aBox[i].style.animation = 
-                'going-left 0.5s forwards, '
-                'fade-out 0.5s forwards';
-            }   //감사합니다 감사합니다ㅋㅋㅋㅋㅋㅋㅋ
-        setTimeout(function() {   
+        let parent = answer.parentNode;
+        parent.style.animation = '';
+        let children = parent.childNodes;
+        for (let i = 0; i < children.length; i++) {
+            children[i].disabled = true;
+        }
+        parent.style.animation = 
+            'fade-out 0.5s 0.4s';
+        setTimeout(function() {
+            select[qIdx] = idx; 
+            a.innerHTML = '';
             goNext();
-        a.innerHTML = '';   
-            
-        }, 1000);
-        
+        }, 800);
     });
 
-    answer.style.animation = 
-        'going-down 0.3s forwards, fade-in 0.3s forwards';
-
-    answer.display = 'none';
+    setTimeout(function() {
+        answer.style.animation = 
+            'going-down 0.25s forwards, fade-in 0.25s forwards';
+    }, 50);
     a.appendChild(answer);
 }
 
-function addClass(elem, className) {
-    elem.className += " "+className;
-}
 
 function goNext() {
-    if (qIdx++ === qnaList.length) {
+    if (qIdx++ === qnaList.length - 1) {
         end();
         return;
     }
-    const qna = document.getElementById('qna');
-    qna.style.display = 'block';
-    console.log(qIdx);
     /*
-    status = document.querySelector('.status');
-    status.style.width = (STATUS * (idx+1))+'%';
+    qna.style.animation = '';
+    qna.style.animation = 
+        'fade-out 0.3s ease-in-out, '+
+        'going-up 0.3s ease-in-out';
+    qna.style.opacity = 0;
     */
+    let status = document.querySelector('.status');
+    status.style.width = (ENDPOINT * (qIdx+1))+'%';
+    
     qNum = qnaList[qIdx];
     const q = document.querySelector('.q');
     q.innerHTML = qNum.q;
     qna.style.animation = 
-        'fade-in 0.3s ease-in-out 0.5s forwards, '+
-        'going-down 0.3s ease-in-out 0.5s forwards';
+        'fade-in 0.3s ease-in-out 0.4s forwards, '+
+        'going-down 0.3s ease-in-out 0.4s forwards';
+        
     setTimeout(function() {
         const endIdx = qNum.a.length-1;
         for (let i in qNum.a) {
-            console.log(i);
             addAnswer(qNum.a[i].answer, i);
         }
-    }, 1000);
+        qna.style.opacity = 1;
+    }, 700);
 }
 
 function begin() {
@@ -76,8 +119,6 @@ function begin() {
         return;
     beginFlag = true;
 
-    const header = document.getElementById('header');
-    const footer = document.getElementById('footer');
     const welcome = document.getElementById('welcome');
 
     header.style.animation = 
@@ -93,6 +134,7 @@ function begin() {
         header.style.display = 'none';
         footer.style.display = 'none';
         welcome.style.display = 'none';
+        qna.style.display = 'block';
         goNext();
     }, 1000);
 }
